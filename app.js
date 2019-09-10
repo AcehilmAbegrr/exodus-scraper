2b853d19a6f2ce6b3be09c3a7f94624283f8baad
Michael Garber <posterfearguy@gmail.com>
2019-09-10 10:52:32 -0400

    print chapter anchor links

--- app.js
+++ app.js
@@ -27,4 +27,14 @@ const print_exodus = async () => {
     const book_of_exodus = $('#mw-content-text', html).text()
     console.log(book_of_exodus)
 }
-print_exodus()
+//print_exodus()
+
+const print_chapter_links = async () => {
+    const html = await request_promise(exodus_wiki)
+    const exodus_chapters = $('#toc > ul:nth-child(3) a', html)
+    const chapter_anchored_links = exodus_chapters.map(
+        (i, chapter) => exodus_wiki.url + chapter.attribs.href
+    )
+    console.log(chapter_anchored_links.toArray())
+}
+print_chapter_links()
